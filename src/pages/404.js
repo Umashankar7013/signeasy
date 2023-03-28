2181da58f52772d0f6234373c27de8d8e6b74ee3
Umashankar <shankar@tilicho.in>
2023-03-28 09:52:17 +0530

    test commit 1

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] flex justify-center items-center">
+      <div className="text-[30px] text-[green]">Processing..</div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
