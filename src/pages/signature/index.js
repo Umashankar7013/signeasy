ddd51abccdffbad3768370407233abd1e525131f
Umashankar <shankar@tilicho.in>
2023-04-06 14:51:04 +0530

    test commit 3

--- src/pages/signature/index.js
+++ src/pages/signature/index.js
@@ -24,7 +24,7 @@ function Signature() {
   };
   const [emails, setEmails] = useState([]);
   const router = useRouter();
-  const selectedItem = JSON.parse(router?.query?.selectedItem);
+  const selectedItem = JSON?.parse(router?.query?.selectedItem);
   console.log(selectedItem, "sel");
 
   const verificationTypeHandler = (type, index) => {
