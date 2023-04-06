2aad0eed03338c32f01a745a2870f3d34a75adba
Umashankar <shankar@tilicho.in>
2023-04-06 14:27:02 +0530

    test commit 3

--- src/pages/signature/index.js
+++ src/pages/signature/index.js
@@ -24,8 +24,8 @@ function Signature() {
   };
   const [emails, setEmails] = useState([]);
   const router = useRouter();
-  const selectedItem = router.query;
-  console.log(selectedItem, "uma");
+  const selectedItem = JSON.parse(router?.query?.selectedItem);
+  console.log(selectedItem);
 
   const verificationTypeHandler = (type, index) => {
     setSignersData((prev) => {
@@ -43,11 +43,11 @@ function Signature() {
       />
       <div className="p-[16px] ml-[17px] border-[1px] mt-[14px] border-[#E0E3EA] rounded-[3px] md:w-[70%]">
         <div className="font-lexend font-[500] leading-[17px] text-[14px]">
-          Healthcare discharge form.pdf
+          {selectedItem?.name}
         </div>
-        <div className="pt-[6px] font-lexend font-[300] leading-[15px] text-[12px] text-[#9DABC0]">
+        {/* <div className="pt-[6px] font-lexend font-[300] leading-[15px] text-[12px] text-[#9DABC0]">
           1 pages
-        </div>
+        </div> */}
       </div>
     </div>
   );
