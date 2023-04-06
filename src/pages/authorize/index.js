bbde13424193ddc4652bac7daecc1b6afcb6aa60
Umashankar <shankar@tilicho.in>
2023-04-06 11:10:40 +0530

    test commit 1

--- src/pages/authorize/index.js
+++ src/pages/authorize/index.js
@@ -73,7 +73,7 @@ const Authorize = () => {
         return;
       }
       const searchParams = new URL(currentUrl).searchParams;
-      console.log(searchParams);
+      console.log(currentUrl);
       const status = searchParams.get("status");
       const userId = searchParams?.get("hubspot_user_id");
       const portalId = searchParams?.get("hubspot_portal_id");
