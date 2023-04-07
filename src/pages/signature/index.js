8db6a9b7fa1fa40bcd1fc7e7a88987512e124b72
Umashankar <shankar@tilicho.in>
2023-04-07 14:52:47 +0530

    test commit 7

--- src/pages/signature/index.js
+++ src/pages/signature/index.js
@@ -286,7 +286,7 @@ function Signature() {
       }
       const searchParams = new URL(currentUrl).searchParams;
       const pending_file_id = searchParams.get("pending_file_id");
-      console.log(currentUrl, pending_file_id);
+      console.log(currentUrl, pending_file_id, "uma");
       if (pending_file_id) {
         await envelopSaveHandler(pending_file_id);
         openNotification({ message: "Success" });
@@ -298,8 +298,9 @@ function Signature() {
   };
 
   useEffect(() => {
+    console.log(location, "locatiob");
     popupObserver();
-  }, [editPopUp]);
+  }, [location]);
 
   return (
     <>
