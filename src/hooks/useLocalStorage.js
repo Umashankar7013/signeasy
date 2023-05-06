a032952f0c1c2345d59fbb65b5f47376cfd7363e
Nagajyothi Prakash <nagajyothi@tilicho.in>
2023-05-06 20:25:14 +0530

    minor change

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -22,7 +22,9 @@ export const useLocalStorage = (key, defaultValue) => {
         localStorage.setItem(key, JSON.stringify(value));
       }
     } else {
-      localStorage.setItem(key, JSON.stringify(value));
+      console.log(value, '----value----')
+      if (key === "JWTtoken" && value === 'undefined') localStorage.setItem(key, "");
+      else localStorage.setItem(key, JSON.stringify(value));
     }
   }, [key, value]);
 
