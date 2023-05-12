5897f400c1b53fbaded2ee2e1e4219fd74cd704c
Nagajyothi Prakash <nagajyothi@tilicho.in>
2023-05-12 15:23:16 +0530

    Small change

--- src/pages/template-mapping/index.js
+++ src/pages/template-mapping/index.js
@@ -49,7 +49,7 @@ function TemplateMapping() {
     const page = searchParams?.get("page");
     setDocParams((prev) => ({ ...prev, authId }));
     console.log(authId === docParams?.authId)
-    if (authId === docParams?.authId) {
+    if (authId !== docParams?.authId) {
       await getApi({
         endUrl: `set-up/auth?authId=${authId}&hubspot_user_id=${userId}&hubspot_portal_id=${portalId}&page=${page}`,
       })
