64e49de524633f80dbe0872249fa658589dabf1c
Umashankar <shankar@tilicho.in>
2023-05-26 21:34:35 +0530

    small change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,21 @@
 import "../../styles/globals.css";
 import { Layout } from "../components/Layout";
 import { ErrorPage } from "../components/ErrorPage";
+import { useEffect, useState } from "react";
+import { Loader } from "../components/Loader";
 
 function MyApp({ Component, pageProps }) {
-  return typeof navigator !== "undefined" && navigator.cookieEnabled ? (
+  const [loading, setLoading] = useState(true);
+  const [cookieEnabled, setCookieEnabled] = useState(true);
+
+  useEffect(() => {
+    setCookieEnabled(navigator.cookieEnabled);
+    setLoading(false);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : cookieEnabled ? (
     <Layout>
       <Component {...pageProps} />
     </Layout>
